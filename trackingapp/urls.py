c2aa60928c0b4f1116d7f83c5e5e99119dee1628
Rodralban17 <alban@gmail.com>
2026-05-15 02:27:26 +0200

    Configure database migrations and initialize backend

--- trackingapp/urls.py
+++ trackingapp/urls.py
@@ -19,8 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    # path('api/', include('api.urls')),
     path('api/auth/', include('users.urls')),
     path('api-auth', include('rest_framework.urls')),
-    path('api/', include('shipments.urls')),
+    # path('api/', include('shipments.urls')),
 ]
